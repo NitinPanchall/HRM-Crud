2a3135a232fd464f8055453f800de2ca1a86dbec
NitinPanchall <mail.nitin.panchal@gmail.com>
2022-01-05 19:27:11 +0530

    commit message

---
+++ README.md
@@ -0,0 +1,7 @@
+# HRM Crud 
+
+This project is updated to [Angular CLI](https://github.com/angular/angular-cli) version 8. This app is simple angular CRUD app which is meant for getting started project. This project can be extended to accomodate any enterprise based features.
+
+
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
